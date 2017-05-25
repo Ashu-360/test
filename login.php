bed15e5ed1ba35cc49a6e7c05f674b171f5c5001
Ashu-360 <princeashugarg@gmail.com>
2017-05-25 12:41:10 +0530

    Register add

--- login.php
+++ login.php
@@ -3,10 +3,12 @@
         <title>My first PHP Website</title>
     </head>
     <body>
-        <?php
-            echo "<p>Hello World!</p>";
-        ?>
-        <a href="login.php"> Click here to login 
-        <a href="register.php"> Click here to register 
+        <h2>Login Page</h2>
+        <a href="firsttesttest.php">Click here to go back<br/><br/>
+        <form action="checklogin.php" method="POST">
+           Enter Username: <input type="text" name="username" required="required" /> <br/>
+           Enter password: <input type="password" name="password" required="required" /> <br/>
+           <input type="submit" value="Login"/>
+        </form>
     </body>
-</html> 
+</html>
