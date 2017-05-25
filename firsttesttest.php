3332797efc2544efbdccd781dae71c183b84b888
Ashu-360 <princeashugarg@gmail.com>
2017-05-25 12:33:40 +0530

    Login

--- firsttesttest.php
+++ firsttesttest.php
@@ -6,7 +6,5 @@
         <?php
             echo "<p>Hello World!</p>";
         ?>
-        <a href="login.php"> Click here to login 
-        <a href="register.php"> Click here to register 
     </body>
 </html> 
